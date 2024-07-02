292873cfc5ef7079675e32eb07b59e37c8396efc
Andrew Charlton <adcharlton@gmail.com>
2024-07-02 14:03:23 +1200

    Update to 5.4 and cleanup

--- Source/DurieHill.Target.cs
+++ Source/DurieHill.Target.cs
@@ -8,7 +8,7 @@ public class DurieHillTarget : TargetRules
 	public DurieHillTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "DurieHill" } );
 	}
